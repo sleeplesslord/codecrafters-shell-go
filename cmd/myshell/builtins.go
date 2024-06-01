44e74fe3e084996000028e5ab0e670789b3d483d
sleeplesslord <me@hellbean.com>
2024-06-01 22:30:32 +0900

    Handle files on path

--- cmd/myshell/builtins.go
+++ cmd/myshell/builtins.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 var builtInHandlers map[string]func(string)
@@ -17,12 +17,12 @@ func init() {
 }
 
 func handleBuiltIn(command string, args string) (ok bool) {
-    handler, ok := builtInHandlers[command]
+	handler, ok := builtInHandlers[command]
 	if ok {
 		handler(args)
 	}
 
-    return
+	return
 }
 
 func exitCommand(args string) {
@@ -37,6 +37,8 @@ func echoCommand(args string) {
 func typeCommand(args string) {
 	if _, ok := builtInHandlers[args]; ok {
 		fmt.Printf("%s is a shell builtin\n", args)
+	} else if path, found := fileInPathVariables(args); found {
+		fmt.Printf("%s is %s/%s\n", args, path, args)
 	} else {
 		fmt.Printf("%s not found\n", args)
 	}
--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,17 +4,42 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-    "strings"
+	"strings"
 )
 
 var pathDirectories []string
 
 func handleCommand(command string, args string) (ok bool) {
-    return handleBuiltIn(command, args)
+	return handleBuiltIn(command, args)
+}
+
+func fileInPath(path string, command string) (found bool) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return false
+	}
+
+	for _, e := range entries {
+		if e.Name() == command {
+			return true
+		}
+	}
+
+	return false
+}
+
+func fileInPathVariables(command string) (path string, found bool) {
+	for _, path := range pathDirectories {
+		if fileInPath(path, command) {
+			return path, true
+		}
+	}
+
+	return "", false
 }
 
 func init() {
-    pathDirectories = strings.Split(os.Getenv("PATH"), ":")
+	pathDirectories = strings.Split(os.Getenv("PATH"), ":")
 }
 
 func main() {
@@ -29,9 +54,9 @@ func main() {
 
 		command, args, _ := strings.Cut(input, " ")
 
-        handled := handleCommand(strings.Trim(command, "\n"), strings.Trim(args, "\n"))
-        if (!handled) {
-            fmt.Printf("%s: command not found\n", command)
-        }
+		handled := handleCommand(strings.Trim(command, "\n"), strings.Trim(args, "\n"))
+		if !handled {
+			fmt.Printf("%s: command not found\n", command)
+		}
 	}
 }
