73c67decdf7c1372c960088dac273d76835fd31f
sleeplesslord <me@hellbean.com>
2024-06-01 21:46:39 +0900

    Handle command with switch

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,27 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-    "strings"
-    "strconv"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-    for {
-        fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-        // Wait for user input
-        input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-        fields := strings.Fields(input)
-        if fields[0] == "exit" {
-            exitCode, _ := strconv.Atoi(fields[1])
-            os.Exit(exitCode)
-        }
-            
+		command, rest, _ := strings.Cut(input, " ")
 
-        fmt.Printf("%s: command not found\n", strings.Trim(input, "\n"))
-    }
+		switch command {
+		case "exit":
+			exitCode, _ := strconv.Atoi(rest)
+			os.Exit(exitCode)
+		case "echo":
+			fmt.Print(rest)
+		default:
+			fmt.Printf("%s: command not found\n", strings.Trim(command, "\n"))
+		}
+	}
 }
