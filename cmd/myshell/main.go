9cb30ae6900863150d2295968d6a19c3ee8cf20e
sleeplesslord <me@hellbean.com>
2024-06-01 21:36:28 +0900

    no commands are found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+    "strings"
 )
 
 func main() {
@@ -14,5 +15,7 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+    fmt.Printf("%s: command not found", strings.Trim(input, "\n"))
 }
