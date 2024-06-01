36c6d7077eccdb14be2b476289d4eceb969d79db
sleeplesslord <me@hellbean.com>
2024-06-01 21:38:08 +0900

    infinite loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,11 +11,12 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+    for {
+        fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+        // Wait for user input
+        input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-    fmt.Printf("%s: command not found\n", strings.Trim(input, "\n"))
+        fmt.Printf("%s: command not found\n", strings.Trim(input, "\n"))
+    }
 }
